d1722f6f0b1188f5fded5430700c71355627f060
Dabrowey <22200354+Dabrowey@users.noreply.github.com>
2025-11-21 19:51:07 +0300

    file.h, settings.c

--- settings.h
+++ settings.h
@@ -1,6 +1,14 @@
-#include "stdbool.h"
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <stdbool.h>
 
 typedef struct {
     int width, height;
     bool fullscreen;
+    char* language;
 } Settings;
+
+int ReadSettings(Settings* settings);
+
+#endif
---
+++ utils/file.c
@@ -0,0 +1,46 @@
+#include "file.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char* ReadFile(char* path)
+{
+    char* data = NULL;
+    FILE* fptr = fopen(path, "r");
+    if (fptr == NULL) 
+    {
+        goto cleanup;
+    }
+    if (fseek(fptr, 0, SEEK_END) != 0)
+    {
+        goto cleanup;
+    }
+    long size = ftell(fptr);
+    if (size == -1L)
+    {
+        goto cleanup;
+    }
+    if (fseek(fptr, 0, SEEK_SET) != 0)
+    {
+        goto cleanup;
+    }
+    data = malloc(sizeof(char) * (size + 1));
+    if (data == NULL)
+    {
+        goto cleanup;
+    }
+    size_t file_read = fread(data, sizeof(char), size, fptr);
+    if (file_read < (size_t)size)
+    {
+        free(data);
+        data = NULL;
+        goto cleanup;
+    }
+    data[size] = '\0';
+
+    cleanup:
+        if (fptr != NULL)
+        {
+            fclose(fptr);
+        }
+        return data;
+}
